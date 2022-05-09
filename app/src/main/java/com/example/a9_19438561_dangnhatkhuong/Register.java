d931c49d2bb1f9083a3aacc62badc2fc11091f65
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-05-09 16:51:57 +0700

    'finish'

--- app/src/main/java/com/example/a9_19438561_dangnhatkhuong/Register.java
+++ app/src/main/java/com/example/a9_19438561_dangnhatkhuong/Register.java
@@ -30,6 +30,7 @@ public class Register extends AppCompatActivity {
         EditText email = findViewById(R.id.txtEmailInput_re);
         EditText password = findViewById(R.id.txtPasswordInput_re);
         EditText rePassword = findViewById(R.id.txtRePasswordInput_re);
+        EditText txtName = findViewById(R.id.txtNameInput_re);
 
         Button btnRegister = findViewById(R.id.btnRegister_re);
         TextView txtLogin_re = findViewById(R.id.txtLogin_re);
@@ -37,11 +38,15 @@ public class Register extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(password.getText().toString().equals(rePassword.getText().toString())){
+                if(txtName.getText().toString().equals("") || email.getText().toString().equals("") || password.getText().toString().equals("") || rePassword.getText().toString().equals("")){
+                    Toast.makeText(Register.this,"Vui lòng nhập đủ thông tin",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(password.getText().toString().equals(rePassword.getText().toString())){
                     createAccount(email.getText().toString(),password.getText().toString());
                 } else{
                     Toast.makeText(Register.this,"Xem lai mat khau",Toast.LENGTH_SHORT).show();
-                    return;
+
                 }
 
             }
